0dd9e82b366df60e3b840ddbc33e5f8713b251bc
unknown <876848785@qq.com>
2021-09-06 23:17:08 +0800

    Site updated: 2021-09-06 23:17:07

--- libs/codeBlock/codeCopy.js
+++ libs/codeBlock/codeCopy.js
@@ -56,7 +56,6 @@
 //     })
 // });
 // 代码块一键复制
-// 代码块一键复制
 $(function () {
     var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
     var $notice = $('<div class="codecopy_notice"></div>')
